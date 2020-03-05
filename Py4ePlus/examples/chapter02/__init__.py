63fd237e3074c6167909fe0e961e0a9f7db95d35
Gattinger, Thomas <Thomas.Gattinger@debeka.de>
2020-03-05 15:27:12 +0100

    Kapitel 3 begonnen

--- Py4ePlus/examples/chapter02/__init__.py
+++ Py4ePlus/examples/chapter02/__init__.py
@@ -4,4 +4,6 @@ Kapital 2 - Variablen
 In diesem Kapitel geht es um Variablen, Datentypen, Schlüsselwörter, Zuweisungen, Ausdrücke und Operationen.
 
 @see: https://www.py4e.com/html3/02-variables
+@see: https://www.python-kurs.eu/python3_variablen.php
+@see: https://www.python-kurs.eu/python3_operatoren.php
 '''
---
+++ Py4ePlus/examples/chapter03/__init__.py
@@ -0,0 +1,8 @@
+'''
+Kapitel 3 - Bedingte Anweisungen
+
+In diesem Kapitel geht es um bedingte Anweisungen in verschiedenen Formen, sowie der Fehlerbehandlung
+
+@see: https://www.py4e.com/html3/03-conditional
+@see: https://www.python-kurs.eu/python3_bedingte_anweisungen.php
+'''
