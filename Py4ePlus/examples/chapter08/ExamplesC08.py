44ea562c36bc2b72cb9c0ace1792f0a3ad2fc16d
morrigan <morrigan@morrigan-laptop>
2020-03-11 10:11:09 +0100

    Kapitel 8 hinzugefügt

--- Py4ePlus/examples/chapter08/ExamplesC08.py
+++ Py4ePlus/examples/chapter08/ExamplesC08.py
@@ -1,6 +1,86 @@
 '''
 Created on 10.03.2020
 
+In diesem Modul sind Beispiele zu folgenden Themen enthalten:
+- Listen
+
 @author: morrigan
 '''
+
+# Erzeugung von Listen
+int_list = [1, 2, 3]
+print("int_list =", int_list)
+mixed_list = ["Eine", "Liste", "mit", 5, ["I", "t", "e", "m", "s"]]
+print("mixed_list =", mixed_list)
+empty_list = []
+print("empty_list =", empty_list)
+
+# Listen können verändert werden
+mutable_list = ["A", "B", "D"]
+print("mutable_list =", mutable_list)
+mutable_list[2] = "C"
+print("mutable_list =", mutable_list)
+
+# Operationen auf Listen
+my_list = [0] * 4
+print("my_list =", my_list)
+my_list[0] = 1
+print("my_list =", my_list)
+my_list = ['a', 'b', 'c', 'd', 'e', 'f']
+print("my_list =", my_list)
+print("my_list[1:3] =", my_list[1:3])
+print("my_list[:4] =", my_list[:4])
+print("my_list[3:] =", my_list[3:])
+print("my_list[:] =", my_list[:])
+
+# Methoden auf Listen
+my_list = ['a', 'b', 'c']
+print("my_list =", my_list)
+my_list.append("d")
+print("my_list =", my_list)
+my_list.extend(['e', 'f'])
+print("my_list =", my_list)
+my_list.sort(reverse=True)
+print("my_list =", my_list)
+item = my_list.pop(1)
+print("my_list = {}, item = {}".format(my_list, item))
+del my_list[0]
+print("my_list =", my_list)
+my_list.remove('d')
+print("my_list =", my_list)
+
+# Functionen für Listen
+nums = [3, 41, 12, 9, 74, 15]
+print("len(nums) =", len(nums))
+print("max(nums) =", max(nums))
+print("min(nums) =", min(nums))
+print("sum(nums) =", sum(nums))
+print("sum(nums)/len(nums) =", sum(nums) / len(nums))
+
+# Listen mit Strings
+words = "what a beautiful day"
+splitted_words = words.split()
+print("splitted_words =", splitted_words)
+delimiter = "_"
+new_words = delimiter.join(splitted_words)
+print("new_words =", new_words)
+
+# Objekte und Werte
+a = "banana"
+b = "banana"
+print("a = {}, b = {} -> a is b = {}".format(a, b, a is b))
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+print("a = {}, b = {} -> a is b = {}".format(a, b, a is b))
+
+# Listen werden bei Funktionen referenziert
+def delete_head(t):
+    del t[0]
+letters = ['a', 'b', 'c']
+print("letters =", letters)  
+delete_head(letters)
+print("letters =", letters)  
+
+
 
--- Py4ePlus/examples/chapter08/ExercisePy4eC08.py
+++ Py4ePlus/examples/chapter08/ExercisePy4eC08.py
@@ -77,4 +77,88 @@ for line in fhand:
     print(words[2])
 print()
 
+# Exercise 4
+task = """Exercise 4: Download a copy of the file www.py4e.com/code3/romeo.txt.  
+Write a program to open the file romeo.txt and read it line by line. For each line, split the line into a list of 
+words using the split function. For each word, check to see if the word is already in a list. If the word is not in 
+the list, add it to the list. When the program completes, sort and print the resulting words in alphabetical order."""
+code = """Enter file: romeo.txt
+['Arise', 'But', 'It', 'Juliet', 'Who', 'already',
+'and', 'breaks', 'east', 'envious', 'fair', 'grief',
+'is', 'kill', 'light', 'moon', 'pale', 'sick', 'soft',
+'sun', 'the', 'through', 'what', 'window',
+'with', 'yonder']"""
+print_exercise(task, code)
+fname = input("Enter file name: ")
+if(len(fname) < 1):
+    fname = "romeo.txt"
+fh = open(DATA_PATH / fname)
+lst = list()
+for line in fh:
+    newWords = line.rstrip().split()
+    for w in newWords:
+        if(w not in lst):
+            lst.append(w)
+
+lst.sort()
+print(lst)
+print()
+
+# Exercise 5
+task = "Exercise 5: Write a program to read through the mail box data and when you find line that starts with 'From', \
+you will split the line into words using the split function. We are interested in who sent the message, which is the \
+second word on the From line."
+code = "From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008"
+print_exercise(task, code)
+fname = input("Enter file name: ")
+if(len(fname) < 1):
+    fname = "mbox-short.txt"
+fh = open(DATA_PATH / fname)
+count = 0
+for line in fh:
+    if("From " not in line): continue
+    email = line.split()[1]
+    print(email)
+    count += 1
+print("There were", count, "lines in the file with From as the first word")
+print()
+
+# Exercise 6
+task = "Exercise 6: Rewrite the program that prompts the user for a list of numbers and prints out the maximum \
+and minimum of the numbers at the end when the user enters 'done'. Write the program to store the numbers the user \
+enters in a list and use the max() and min() functions to compute the maximum and minimum numbers after the loop \
+completes."
+code = """Enter a number: 6
+Enter a number: 2
+Enter a number: 9
+Enter a number: 3
+Enter a number: 5
+Enter a number: done
+Maximum: 9.0
+Minimum: 2.0"""
+print_exercise(task, code)
+largest = None
+smallest = None
+while True:
+    command = input("Enter a number: ")
+    if(command == "done"): break
+
+    num = 0    
+    try:
+        num = float(command)
+    except:
+        print("Invalid input")
+        continue
+
+    if(largest is None or largest < num):
+        largest = num
+    if(smallest is None or smallest > num):
+        smallest = num    
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
+print()
+
+
+
 
